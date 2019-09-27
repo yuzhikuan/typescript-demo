bbbb8e2d42b3fd6c5e69273a1ff1904652b14b4a
zkyucn <zkyucn@zkyucndeMacBook-Pro.local>
2019-09-28 01:01:42 +0800

    安装typescript，编译tsc命令

---
+++ index.js
@@ -0,0 +1,3 @@
+var add = function (num1, num2) { return num1 + num2; };
+var res = add(1, 2);
+console.log(res);
---
+++ index.ts
@@ -0,0 +1,5 @@
+const add = (num1: number, num2: number): number => num1 + num2;
+
+const res = add(1, 2);
+
+console.log(res);
